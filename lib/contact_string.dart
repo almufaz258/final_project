1f0812540926fa2b644fc96f57f877c1b54268f3
Albina M <103380907+almufaz258@users.noreply.github.com>
2022-04-20 00:16:52 +0500

    final-project

---
+++ lib/contact_string.dart
@@ -0,0 +1,14 @@
+import 'dart:core';
+
+const String URL_GET_USERS_LIST = "https://jsonplaceholder.typicode.com/users";
+const String URL_GET_USERS_TODOS_LIST = 'https://jsonplaceholder.typicode.com/todos';
+
+class Strings{
+  static const String userName = '9272383702';
+  static const String userPass = 'Password';
+  static const String validName = 'Обязательное поле не заполнено';
+  static const String validNameLength = 'Введите 10 цифр номера телефона';
+  static const String fieldPhone = 'Телефон';
+  static const String fieldPass = 'Пароль';
+  static const String validPass = 'Обязательное поле не заполнено';
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:final_project/start_screen.dart';
+import 'package:final_project/user_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:final_project/task_screen.dart';
+import 'final_theme.dart';
+
+
+void main() {
+  runApp(const MyApp());}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const RegScreen(),
+        '/users': (context) => const UserScreen(),
+        '/tasks': (context) => const TaskMainScreen(),
+      },
+      title: 'Заголовок',
+      theme: myTheme(),
+    );
+  }
+}
+
